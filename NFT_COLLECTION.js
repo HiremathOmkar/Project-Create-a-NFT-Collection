b3cd1ebde39efe61f4c929c84c89076c368f5501
Omkar Hiremath <134164664+HiremathOmkar@users.noreply.github.com>
2023-06-21 14:07:13 +0530

    Update and rename NFT_COLLECTION to NFT_COLLECTION.js

--- NFT_COLLECTION.js
+++ NFT_COLLECTION.js
@@ -10,22 +10,40 @@ Assessment Requirements
 
 // create a variable to hold your NFT's
 
+const NFTs=[];
+
 // this function will take in some values as parameters, create an
 // NFT object using the parameters passed to it for its metadata, 
 // and store it in the variable above.
-function mintNFT () {
 
+function mintNFT (_name,_eyecolor,_shirtcolor,_shoescolor) {
+const NFT={
+    "name":_name,
+    "eyecolor":_eyecolor,
+    "shirtcolor":_shirtcolor,
+    "shoescolor":_shoescolor
+}
+NFTs.push(NFT);
+console.log("Minting NFT"+":"+ NFT.name);
 }
 
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
-function listNFTs () {
 
+function listNFTs () {
+for(let i=0;i<NFTs.length;i++){
+    console.log(NFTs[i]);
+    }
 }
 
 // print the total number of NFTs we have minted to the console
-function getTotalSupply() {
 
+function getTotalSupply() {
+    console.log(NFTs.length);
 }
 
 // call your functions below this line
+mintNFT("Sam","blue","red","black");
+mintNFT("Ron","black","blue","brown");
+listNFTs();
+getTotalSupply();
